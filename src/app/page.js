fc31aa6054d61c3d98fce3ffd7a48e0f2b72ca22
Manav Gohil <gohilmanav2005@gmail.com>
2025-06-05 21:15:50 +0530

    some changes

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import Footer from '@/components/footer';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-white text-black px-15">
+    <div className="min-h-screen bg-white text-black px-15 overflow-hidden">
       <Navbar />  
         
       <div className='relative'>
@@ -68,7 +68,7 @@ export default function Home() {
           <img src='product.png' alt="product's overview"/>
         </div>
 
-        <button className='mt-15 rounded-lg bg-black text-white w-40 py-4 mx-auto '>View Course</button>
+        <button className='mt-15 rounded-lg bg-black text-white w-40 py-4 mx-auto hover:cursor-pointer'>View Course</button>
       </div>
       
 
@@ -79,7 +79,7 @@ export default function Home() {
         <div className='pt-4 flex justify-center'>
           <div className='flex rounded-2xl border border-gray-300 overflow-hidden'>
             <button className='flex-1 py-2 w-60 text-white text-sm font-medium bg-gradient-to-bl from-[#2D3BF2] to-[#B832E9]'>Data Vidhya Projects</button>
-            <button className='flex-1 py-2 text-sm text-[#33333366] font-medium'>Students Review</button>
+            <button className='flex-1 py-2 text-sm text-[#33333366] font-medium hover:cursor-pointer'>Students Review</button>
           </div>
         </div>
         <div className='flex flex-wrap justify-center gap-5 pt-12'>
@@ -88,7 +88,7 @@ export default function Home() {
             <ProjectCard image={"project.png"} project_name={"Project Name"} Airflow={"AirFlow"} description={"Learn to automate, schedule, and monitor data pipelines with Apache Airflow. This course covers the basics, building efficient DAGs, and real-world projects on AWS & GCP. Perfect for anyone looking to manage modern data workflows easily"} />
         </div>
         <div className='text-center pt-12'>
-          <button className='text-white bg-black rounded-lg w-40 mx-auto py-2'>View All Projects</button>        
+          <button className='text-white bg-black rounded-lg w-40 mx-auto py-2 hover:cursor-pointer'>View All Projects</button>        
         </div>
       </div>
 
@@ -167,10 +167,10 @@ export default function Home() {
       </div>
 
 
-      <div className='flex flex-col bg-[#F7F7FB]'>
+      <div className='w-screen flex flex-col bg-[#F7F7FB]'>
           <div className='text-center text-4xl font-bold mt-30'>FAQs</div>
           <div className='text-center pt-5'>Find answers to your most pressing questions about our data engineering courses and platform.</div>
-          <div className='pt-15 flex flex-col mb-30 '>
+          <div className='pt-15 flex flex-col mb-40 '>
               <FAQcard question={"What is Data engineering?"}/>
               <FAQcard question={"Who should take courses?"}/>
               <FAQcard question={"What is the platform?"}/>
